fbc9d58dd47fac91045231bde527987228747d79
appu <apparao@vtiger.com>
2023-10-19 10:12:05 +0530

    #1783::Appu::Layout Editor: Default Value is cleared If fieldProperty is enabled/disabled (using a switch UI)

--- modules/Settings/LayoutEditor/actions/Field.php
+++ modules/Settings/LayoutEditor/actions/Field.php
@@ -96,10 +96,13 @@ class Settings_LayoutEditor_Field_Action extends Settings_Vtiger_Index_Action {
         if(!empty($massEditable)){
             $fieldInstance->set('masseditable', $massEditable);
         }
-
-		$defaultValue = decode_html($request->get('fieldDefaultValue'));
-		$fieldInstance->set('defaultvalue', $defaultValue);
-		$response = new Vtiger_Response();
+        
+        $defaultValue = $fieldInstance->get('defaultvalue');
+        if(!empty($request->get('fieldDefaultValue'))) {
+            $defaultValue = decode_html($request->get('fieldDefaultValue'));
+            $fieldInstance->set('defaultvalue', $defaultValue);
+        }
+	$response = new Vtiger_Response();
         try{
             $fieldInstance->save();
 			$fieldInstance = Settings_LayoutEditor_Field_Model::getInstance($fieldId);
