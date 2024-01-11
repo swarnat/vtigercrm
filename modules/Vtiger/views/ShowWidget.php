f24613ab48776e999a3fbb3831bc091fc2163a95
kaushik.p <kaushik.p@vtigersolutions.com>
2024-01-11 12:40:41 +0530

    #Fixes::157669688::kaushik::Notebook widget data not displaying over Dashboard

--- modules/Vtiger/views/ShowWidget.php
+++ modules/Vtiger/views/ShowWidget.php
@@ -47,7 +47,7 @@ class Vtiger_ShowWidget_View extends Vtiger_IndexAjax_View {
 						$widget->set('data', $request->get('data'));
 					}
 		    		$widget->add();
-		    		$request->set('widgetid', $widget->get('id'));
+				  //$request->set('widgetid', $widget->get('id'));
 				}
 				
 				//Date conversion from user format to database format
