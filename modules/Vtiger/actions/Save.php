4df4723faa2aabbae2f9c147422cf6fede6ad0d1
yogeshwar.g <yogeshwar.g@vtigersolutions.com>
2024-01-24 13:28:56 +0530

    #Fixes::157692300::Yogeshwar::In DeatailView Multi-combobox custom Field issue

--- modules/Vtiger/actions/Save.php
+++ modules/Vtiger/actions/Save.php
@@ -156,6 +156,9 @@ class Vtiger_Save_Action extends Vtiger_Action_Controller {
 		$fieldModelList = $moduleModel->getFields();
 		foreach ($fieldModelList as $fieldName => $fieldModel) {
 			$fieldValue = $request->get($fieldName, null);
+			if(is_array($fieldValue)){
+				$fieldValue=implode(' |##| ',$fieldValue);
+			}
 			$fieldDataType = $fieldModel->getFieldDataType();
 			if($fieldDataType == 'time' && $fieldValue !== null){
 				$fieldValue = Vtiger_Time_UIType::getTimeValueWithSeconds($fieldValue);
