0fb531ee97b9fee6b361c371a1cb1281a0e4381d
madhurajshanbhogh <1rn19is075.madhusr@gmail.com>
2024-02-09 18:07:46 +0530

    #Fixes::158349044::madhusr::When we add the column in the RTE field it is not displaying properly in detail view

--- modules/Documents/models/Field.php
+++ modules/Documents/models/Field.php
@@ -44,5 +44,16 @@ class Documents_Field_Model extends Vtiger_Field_Model {
         }
         return false;
     }
+
+	public function isAjaxEditable() {
+		$result = parent::isAjaxEditable();
+		if($result && $this->get('uitype') == 19) {
+			return false;
+		} else if(!$result) {
+			return false;
+		} else {
+		return true;
+		}
+	}
     
 }
