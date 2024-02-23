5e31f61ab37c905dc6fa3b1bf273f221f3a1e3ca
yogeshwar.g <yogeshwar.g@vtigersolutions.com>
2024-02-23 16:23:38 +0530

    #Fixes::158450960::Yogeshwar::When we unlink the SO from the Organization,SO is getting deleted permanently;

--- modules/SalesOrder/SalesOrder.php
+++ modules/SalesOrder/SalesOrder.php
@@ -421,7 +421,8 @@ class SalesOrder extends CRMEntity {
 		if(empty($return_module) || empty($return_id)) return;
 
 		if($return_module == 'Accounts') {
-			$this->trash('SalesOrder',$id);
+			$relation_query = 'UPDATE vtiger_salesorder SET accountid=? WHERE salesorderid=?';
+			$this->db->pquery($relation_query, array(null, $id));
 		}
 		elseif($return_module == 'Quotes') {
 			$relation_query = 'UPDATE vtiger_salesorder SET quoteid=? WHERE salesorderid=?';
