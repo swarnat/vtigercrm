7796c4dfcc358cf5a6b867ee95c99fcbeebd47c1
madhu sr <madhu.sr@vtigersolutions.com>
2024-04-15 10:12:22 +0000

    #Fixes::159015734::madhusr::Canceled status is not updating in the project task module

--- modules/Migration/schema/811_to_812.php
+++ modules/Migration/schema/811_to_812.php
@@ -1,10 +1,5 @@
 <?php
 if (defined('VTIGER_UPGRADE')) {
-ini_set('error_reporting', E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_WARNING);
-ini_set('display_errors', 'On');
-include_once 'include/utils/utils.php';
-include_once 'includes/runtime/Viewer.php';
-include_once 'includes/runtime/LanguageHandler.php';
 global $adb, $current_user;
     $db = PearDatabase::getInstance();
 
