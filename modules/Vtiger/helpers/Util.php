e32448d5155c3972911988cf0cd489d7858c3e34
madhu sr <madhu.sr@vtigersolutions.com>
2024-04-17 05:46:52 +0000

    Fixes: Handled the formating date to string when date comes in format dd/mm/yyyy

--- modules/Vtiger/helpers/Util.php
+++ modules/Vtiger/helpers/Util.php
@@ -278,6 +278,7 @@ class Vtiger_Util_Helper {
 		if ($currentUser->get('date_format') === 'mm-dd-yyyy') {
 			$dateInUserFormat = str_replace('-', '/', $dateInUserFormat);
 		} else if ($currentUser->get('date_format') === 'dd/mm/yyyy'){
+			// strtotime expects the format m/d/y making changes to its convenient
 			$dateArray = explode('/', $dateInUserFormat);
 			$temp = $dateArray[0];
 			$dateArray[0] = $dateArray[1];
