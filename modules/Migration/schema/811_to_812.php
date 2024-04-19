ef4d64f409c496217197a824b7e811b96f3f68d1
Prasad <prasad@vtiger.com>
2024-04-19 17:17:02 +0530

    Fixes #1859: Add ModComments relation to Inventory modules

--- modules/Migration/schema/811_to_812.php
+++ modules/Migration/schema/811_to_812.php
@@ -8,4 +8,18 @@ if (defined('VTIGER_UPGRADE')) {
 
     // Trim the space in value.
     $db->pquery('UPDATE vtiger_projecttaskstatus set projecttaskstatus = "Canceled" where projecttaskstatus = "Canceled "', array());
+
+    // Ensure related-tab for ModComments on Inventory modules (if missed in previous migration)
+    $modCommentsInstance = Vtiger_Module_Model::getInstance('ModComments');
+	$modCommentFieldInstance = Vtiger_Field_Model::getInstance('related_to', $modCommentsInstance);
+	foreach(getInventoryModules() as $refModuleName) {
+    	$refModuleModel = Vtiger_Module_Model::getInstance($refModuleName);
+    	$rs = $db->pquery("SELECT 1 FROM vtiger_relatedlists WHERE tabid=? and related_tabid=? and relationfieldid=? limit 1", array(
+       		$refModuleModel->id, $modCommentsInstance->id, $modCommentFieldInstance->id
+    	));
+    	if (!$db->num_rows($rs)) {
+        	$refModuleModel->setRelatedList($modCommentsInstance, "ModComments", '', 'get_comments', $modCommentFieldInstance->id);
+    	}
+	}
+
 }
