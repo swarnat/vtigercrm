4882dff5a8d1a5ba26c61a9913d128ee7ce71da3
yogeshwar <yogeshwar.g@vtigersolutions.com>
2024-04-29 00:06:27 +0530

    Fixes

--- modules/Vtiger/views/FindDuplicates.php
+++ modules/Vtiger/views/FindDuplicates.php
@@ -8,20 +8,26 @@
  * All Rights Reserved.
  ************************************************************************************/
 
-class Vtiger_FindDuplicates_View extends Vtiger_List_View {
+class Vtiger_FindDuplicates_View extends Vtiger_List_View
+{
 
-	function preProcess(Vtiger_Request $request, $display = true) {
-		$viewer = $this->getViewer ($request);
+	protected $fncalled = false;
+
+	function preProcess(Vtiger_Request $request, $display = true)
+	{
+		$viewer = $this->getViewer($request);
 		$this->initializeListViewContents($request, $viewer);
-        parent::preProcess($request, $display);
+		parent::preProcess($request, $display);
 	}
 
-	public function preProcessTplName(Vtiger_Request $request) {
+	public function preProcessTplName(Vtiger_Request $request)
+	{
 		return 'FindDuplicatePreProcess.tpl';
 	}
 
-	function process (Vtiger_Request $request) {
-		$viewer = $this->getViewer ($request);
+	function process(Vtiger_Request $request)
+	{
+		$viewer = $this->getViewer($request);
 		$moduleName = $request->getModule();
 		$moduleModel = Vtiger_Module_Model::getInstance($moduleName);
 		$this->initializeListViewContents($request, $viewer);
@@ -37,7 +43,8 @@ class Vtiger_FindDuplicates_View extends Vtiger_List_View {
 	 * @param Vtiger_Request $request
 	 * @return <Array> - List of Vtiger_JsScript_Model instances
 	 */
-	function getHeaderScripts(Vtiger_Request $request) {
+	function getHeaderScripts(Vtiger_Request $request)
+	{
 		$headerScriptInstances = parent::getHeaderScripts($request);
 		$moduleName = $request->getModule();
 
@@ -54,85 +61,92 @@ class Vtiger_FindDuplicates_View extends Vtiger_List_View {
 	/*
 	 * Function to initialize the required data in smarty to display the List View Contents
 	 */
-	public function initializeListViewContents(Vtiger_Request $request, Vtiger_Viewer $viewer) {
-		$currentUser = vglobal('current_user');
-		$viewer = $this->getViewer ($request);
-		$module = $request->getModule();
-		$moduleModel = Vtiger_Module_Model::getInstance($module);
+	public function initializeListViewContents(Vtiger_Request $request, Vtiger_Viewer $viewer)
+	{
 
-		$massActionLinks = array();
-		$userPrivilegesModel = Users_Privileges_Model::getCurrentUserPrivilegesModel();
-		if ($userPrivilegesModel->hasModuleActionPermission($moduleModel->getId(), 'Delete')) {
-			$massActionLink = array(
-				'linktype' => 'LISTVIEWBASIC',
-				'linklabel' => 'LBL_DELETE',
-				'linkurl' => 'Javascript:Vtiger_FindDuplicates_Js.massDeleteRecords("index.php?module='.$module.'&action=MassDelete");',
-				'linkicon' => ''
-			);
-			$massActionLinks[] = Vtiger_Link_Model::getInstanceFromValues($massActionLink);
+		if ($this->fncalled == false) {
+			$currentUser = vglobal('current_user');
+			$viewer = $this->getViewer($request);
+			$module = $request->getModule();
+			$moduleModel = Vtiger_Module_Model::getInstance($module);
+
+			$massActionLinks = array();
+			$userPrivilegesModel = Users_Privileges_Model::getCurrentUserPrivilegesModel();
+			if ($userPrivilegesModel->hasModuleActionPermission($moduleModel->getId(), 'Delete')) {
+				$massActionLink = array(
+					'linktype' => 'LISTVIEWBASIC',
+					'linklabel' => 'LBL_DELETE',
+					'linkurl' => 'Javascript:Vtiger_FindDuplicates_Js.massDeleteRecords("index.php?module=' . $module . '&action=MassDelete");',
+					'linkicon' => ''
+				);
+				$massActionLinks[] = Vtiger_Link_Model::getInstanceFromValues($massActionLink);
+			}
+
+			$viewer->assign('CURRENT_USER_PRIVILAGES_MODEL', $userPrivilegesModel);
+			$viewer->assign('LISTVIEW_LINKS', $massActionLinks);
+			$viewer->assign('MODULE_MODEL', $moduleModel);
+
+			$pageNumber = $request->get('page');
+			if (empty($pageNumber)) {
+				$pageNumber = '1';
+			}
+			if (!$this->pagingModel) {
+				$pagingModel = new Vtiger_Paging_Model();
+				$this->pagingModel = $pagingModel;
+			} else {
+				$pagingModel = $this->pagingModel;
+			}
+			$pagingModel->set('page', $pageNumber);
+			$duplicateSearchFields = $request->get('fields');
+			$dataModelInstance = Vtiger_FindDuplicate_Model::getInstance($module);
+			$dataModelInstance->set('fields', $duplicateSearchFields);
+
+			$ignoreEmpty = $request->get('ignoreEmpty');
+			$ignoreEmptyValue = false;
+			if ($ignoreEmpty == 'on' || $ignoreEmpty == 'true' || $ignoreEmpty == '1') $ignoreEmptyValue = true;
+			$dataModelInstance->set('ignoreEmpty', $ignoreEmptyValue);
+
+			if (!$this->listViewEntries) {
+				$this->listViewEntries = $dataModelInstance->getListViewEntries($pagingModel);
+			}
+
+			if (!$this->listViewHeaders) {
+				$this->listViewHeaders = $dataModelInstance->getListViewHeaders();
+			}
+			if (!$this->rows) {
+				$this->rows = $dataModelInstance->getRecordCount();
+				$viewer->assign('TOTAL_COUNT', $this->rows);
+			}
+
+			$viewer->assign('IGNORE_EMPTY', $ignoreEmpty);
+			$viewer->assign('LISTVIEW_ENTRIES_COUNT', $pagingModel->recordCount);
+			$viewer->assign('LISTVIEW_HEADERS', $this->listViewHeaders);
+			$viewer->assign('LISTVIEW_ENTRIES', $this->listViewEntries);
+			$viewer->assign('PAGING_MODEL', $pagingModel);
+			$viewer->assign('PAGE_NUMBER', $pageNumber);
+			$viewer->assign('MODULE', $module);
+			$viewer->assign('DUPLICATE_SEARCH_FIELDS', $duplicateSearchFields);
+
+			$customViewModel = CustomView_Record_Model::getAllFilterByModule($module);
+			$viewer->assign('VIEW_NAME', $customViewModel->getId());
+
+			$this->fncalled = true;
 		}
-
-		$viewer->assign('CURRENT_USER_PRIVILAGES_MODEL', $userPrivilegesModel);
-		$viewer->assign('LISTVIEW_LINKS', $massActionLinks);
-		$viewer->assign('MODULE_MODEL', $moduleModel);
-
-		$pageNumber = $request->get('page');
-		if(empty($pageNumber)){
-			$pageNumber = '1';
-		}
-        if (!$this->pagingModel) {
-            $pagingModel = new Vtiger_Paging_Model();
-            $this->pagingModel = $pagingModel;
-        } else {
-            $pagingModel = $this->pagingModel;
-        }
-        $pagingModel->set('page', $pageNumber);
-		$duplicateSearchFields = $request->get('fields');
-		$dataModelInstance = Vtiger_FindDuplicate_Model::getInstance($module);
-		$dataModelInstance->set('fields', $duplicateSearchFields);
-
-		$ignoreEmpty = $request->get('ignoreEmpty');
-		$ignoreEmptyValue = false;
-		if($ignoreEmpty == 'on' || $ignoreEmpty == 'true' || $ignoreEmpty == '1') $ignoreEmptyValue = true;
-		$dataModelInstance->set('ignoreEmpty', $ignoreEmptyValue);
-
-		if(!$this->listViewEntries) {
-			$this->listViewEntries = $dataModelInstance->getListViewEntries($pagingModel);
-		}
-
-		if(!$this->listViewHeaders){
-			$this->listViewHeaders = $dataModelInstance->getListViewHeaders();
-		}
-		if(!$this->rows) {
-			$this->rows = $dataModelInstance->getRecordCount();
-			$viewer->assign('TOTAL_COUNT', $this->rows);
-		}
-        
-		$viewer->assign('IGNORE_EMPTY', $ignoreEmpty);
-		$viewer->assign('LISTVIEW_ENTRIES_COUNT', $pagingModel->recordCount);
-		$viewer->assign('LISTVIEW_HEADERS', $this->listViewHeaders);
-		$viewer->assign('LISTVIEW_ENTRIES', $this->listViewEntries);
-		$viewer->assign('PAGING_MODEL', $pagingModel);
-		$viewer->assign('PAGE_NUMBER',$pageNumber);
-		$viewer->assign('MODULE', $module);
-		$viewer->assign('DUPLICATE_SEARCH_FIELDS', $duplicateSearchFields);
-
-		$customViewModel = CustomView_Record_Model::getAllFilterByModule($module);
-		$viewer->assign('VIEW_NAME', $customViewModel->getId());
 	}
 
 	/**
 	 * Function returns the number of records for the current filter
 	 * @param Vtiger_Request $request
 	 */
-	function getRecordsCount(Vtiger_Request $request) {
+	function getRecordsCount(Vtiger_Request $request)
+	{
 		$moduleName = $request->getModule();
 		$duplicateSearchFields = $request->get('fields');
 		$dataModelInstance = Vtiger_FindDuplicate_Model::getInstance($moduleName);
 
 		$ignoreEmpty = $request->get('ignoreEmpty');
 		$ignoreEmptyValue = false;
-		if($ignoreEmpty == 'on' || $ignoreEmpty == 'true' || $ignoreEmpty == '1') $ignoreEmptyValue = true;
+		if ($ignoreEmpty == 'on' || $ignoreEmpty == 'true' || $ignoreEmpty == '1') $ignoreEmptyValue = true;
 		$dataModelInstance->set('ignoreEmpty', $ignoreEmptyValue);
 
 		$dataModelInstance->set('fields', $duplicateSearchFields);
