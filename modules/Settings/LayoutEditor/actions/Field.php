cd4c0325b8ebdebb64eb7a05c6be9ace3675537a
yogeshwar <yogeshwar.g@vtigersolutions.com>
2024-04-29 01:10:18 +0530

    Fixes:fixed method being called by a null object

--- modules/Settings/LayoutEditor/actions/Field.php
+++ modules/Settings/LayoutEditor/actions/Field.php
@@ -210,10 +210,10 @@ class Settings_LayoutEditor_Field_Action extends Settings_Vtiger_Index_Action {
                     if ($defaultValue && $fieldInfo['type'] == 'date') {
                         $defaultValue = DateTimeField::convertToUserFormat($defaultValue);
                     } else if (!$defaultValue) {
-                        $defaultValue = $fieldInstance->getDisplayValue($defaultValue);
+                        $defaultValue = $fieldInfo->getDisplayValue($defaultValue);
                     } else if (is_array($defaultValue)) {
                         foreach ($defaultValue as $key => $value) {
-                            $defaultValue[$key] = $fieldInstance->getDisplayValue($value);
+                            $defaultValue[$key] = $fieldInfo>getDisplayValue($value);
                         }
                         $defaultValue = Zend_Json::encode($defaultValue);
                     }
