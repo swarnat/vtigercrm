2185c86544dddeed16cd55d3416f871331de8de1
yogeshwar <yogeshwar.g@vtigersolutions.com>
2024-06-04 16:07:14 +0530

    E-all Settings -Other Settings - Mail Converter

--- include/database/PearDatabase.php
+++ include/database/PearDatabase.php
@@ -531,7 +531,7 @@ class PearDatabase{
 
     function getRowCount(&$result){
 		global $log;
-		$rows='';
+		$rows = 0;
 		if(isset($result) && !empty($result))
 		    $rows= $result->RecordCount();
 		return $rows;
