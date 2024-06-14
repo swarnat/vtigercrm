51a2c126f6cc72285b2957a1e17149c6678d69b6
Prasad <prasad@vtiger.com>
2024-06-14 19:15:20 +0530

    Fix for division-by-zero php 8.x when computing price by conversion rate

--- include/utils/InventoryUtils.php
+++ include/utils/InventoryUtils.php
@@ -1200,7 +1200,7 @@ function getBaseConversionRateForProduct($productid, $mode='edit', $module='Prod
 	$res = $adb->pquery($sql, $params);
 	$conv_rate = $adb->query_result($res, 0, 'conversion_rate');
 
-	return 1 / $conv_rate;
+	return $conv_rate ? (1 / $conv_rate) : 1;
 }
 
 /**	Function used to get the prices for the given list of products based in the specified currency
